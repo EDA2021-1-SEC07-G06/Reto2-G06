af56717ea45bc9e379222cf9f1ea5b16650a5e30
ObedCabanzo <jo.cabanzo@uniandes.edu.co>
2021-04-15 00:10:43 -0500

    Entrega Final

--- App/controller.py
+++ App/controller.py
@@ -68,7 +68,7 @@ def loadVideos(catalog):
     """
     Carga los videos del archivo.
     """
-    videosfile = cf.data_dir + 'videos-small.csv'
+    videosfile = cf.data_dir + 'videos-30pct.csv'
     input_file = csv.DictReader(open(videosfile, encoding='utf-8'))
     for video in input_file:
         model.addVideo(catalog, video)
